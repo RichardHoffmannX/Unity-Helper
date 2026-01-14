afc4b960c70a4b9f63d002d67f6afe79da10dfbe
Richard Hoffmann <95046844+RichardHoffmannX@users.noreply.github.com>
2026-01-14 02:33:30 +0100

    Add target and button to ActiveStateToggler
    Extended functionality

--- ActiveStateToggler.cs
+++ ActiveStateToggler.cs
@@ -3,7 +3,17 @@ using System.Collections;
 
 public class ActiveStateToggler : MonoBehaviour {
 
+ 	public GameObject target;
+ 	public Button button;
+ 	public bool isActiveOnStart = false;
+
+	void Start()
+	{
+		if(button) button.onClick.AddListener(ToggleObjectActive);
+		target.SetActive(isActiveOnStart);
+	}
+	
 	public void ToggleActive () {
-		gameObject.SetActive (!gameObject.activeSelf);
+		target.SetActive (!target.activeSelf);
 	}
 }
